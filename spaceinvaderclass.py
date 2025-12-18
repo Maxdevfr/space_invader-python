c84c67d702e23e0443e21bdfa41478cb444097c0
Max <maxence.perales@gmail.com>
2025-12-18 13:13:58 +0100

    fonctionnement missiles

--- spaceinvaderclass.py
+++ spaceinvaderclass.py
@@ -39,62 +39,56 @@ class Ship:
 
 class App:
     def __init__(self):
-        """
-        Init fenetre + elements
-        """
-        pyxel.init(120,200)
-        self.ship = Ship(60,180)
-        pyxel.run(self.update,self.draw)
-        self.Missile=Missile(60,180)
-        self.Missile=[]
+        pyxel.init(120, 200)
+        self.ship = Ship(60, 180)
+
+      
+        self.missiles = []
+
+        pyxel.run(self.update, self.draw)
+
     def update(self):
-        """
-        Mise a jour des positions et des etats
-        Pas d'affichage ici
-        """
         if pyxel.btn(pyxel.KEY_RIGHT):
-            self.ship.move(1,0)
-          
+            self.ship.move(1, 0)
+
         if pyxel.btn(pyxel.KEY_LEFT):
-            self.ship.move(-1,0)
-        
-        if pyxel.btn(pyxel.KEY_SPACE):
-            self.Missile.move(0,1)
-            
+            self.ship.move(-1, 0)
+
+       
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            self.missiles.append(
+                Missile(self.ship.x, self.ship.y - 6)
+            )
+
+     
+        for missile in self.missiles[:]:
+            missile.move()
+            if missile.y < 0:
+                self.missiles.remove(missile)
+
     def draw(self):
-        """
-        on affiche les elements
-        """
         pyxel.cls(0)
         self.ship.draw()
-        
-        
+        for missile in self.missiles:
+            missile.draw()
+
+
+
 class Missile:
-    
-    def __init__(self,x,y,taille=2):
-        """
-        Caracteristique du vaisseau.
-        c'est un carre dans un premier temps'
-        
-        """
-        self.x=x
-        self.y=y
-        self.taille=taille
-        
-        self.Missile=[]
+    def __init__(self, x, y, taille=2):
+        self.x = x
+        self.y = y
+        self.taille = taille
+        self.vitesse = -2  # vers le haut
+
+    def move(self):
+        self.y += self.vitesse
+
     def draw(self):
-        """
-  
-    
-        """
-        decal=self.taille//2
-        pyxel.rectb(self.x - decal, self.y - decal,self.taille,self.taille,7)
-    def move (self,dy):
-        """
-       
-        """
-        self.x+=x
-        self.y+=dy
-       
+        decal = self.taille // 2
+        pyxel.rect(self.x - decal, self.y - decal,
+                   self.taille, self.taille, 8)
+
 
 App()      
+
